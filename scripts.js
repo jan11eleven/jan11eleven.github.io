f2b8a9ef9ba761ef5f8bf57740a4b581973b4702
Jan Eleven Salvador <jan.salvador@pearlpay.io>
2022-11-16 14:24:17 +0800

    updated portfolio

--- scripts.js
+++ scripts.js
@@ -1,29 +1,6 @@
-$(document).ready(function () {
-  $(".sidenav").sidenav();
-  $(".carousel.carousel-slider").carousel({
-    fullWidth: true,
-    indicators: true,
-  });
-});
+var options = {
+  strings: ['Software Engineer'],
+  typeSpeed: 80,
+};
 
-const nav = document.querySelector("nav");
-let lastScrollTop = 0;
-window.addEventListener(
-  "scroll",
-  function () {
-    var st = window.pageYOffset || document.documentElement.scrollTop;
-    if (st < lastScrollTop) {
-      nav.classList.add("show-nav");
-      document.querySelector("nav").classList.remove("hide-nav");
-    } else if (st > lastScrollTop) {
-      nav.classList.add("hide-nav");
-      document.querySelector("nav").classList.remove("show-nav");
-    }
-    if (window.scrollY == 0) {
-      nav.classList.remove("show-nav");
-      nav.classList.remove("hide-nav");
-    }
-    lastScrollTop = st <= 0 ? 0 : st;
-  },
-  false
-);
+var typed = new Typed('.myText', options);
